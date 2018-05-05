de6a22c38c215007a6b5f6e406df41d93182d64a
Cem Bassoy <cem.bassoy@iosb.fraunhofer.de>
2018-05-05 14:09:30 +0200

    adding fixture to the extent unit test.

--- test/tensor/test_extents.cpp
+++ test/tensor/test_extents.cpp
@@ -6,78 +6,110 @@
 
 #include <boost/test/unit_test.hpp>
 #include <boost/numeric/ublas/tensor/extents.hpp>
+#include <vector>
 
 BOOST_AUTO_TEST_SUITE ( test_extents );
 
-BOOST_AUTO_TEST_CASE(test_extents_ctor,
+
+struct fixture {
+	using extents_type = boost::numeric::ublas::extents;
+	fixture() : extents{
+								extents_type{},    // 0
+								extents_type{1},   // 1
+								extents_type{2},   // 2
+								extents_type{1,1}, // 3
+								extents_type{1,2}, // 4
+								extents_type{2,1}, // 5
+								extents_type{2,3}, // 6
+								extents_type{2,3,1}, // 7
+								extents_type{1,2,3}, // 8
+								extents_type{4,2,3}}  // 9
+	{}
+	std::vector<extents_type> extents;
+};
+
+
+BOOST_FIXTURE_TEST_CASE(test_extents_ctor, fixture,
 										 *boost::unit_test::label("extents")
 										 *boost::unit_test::label("constructor"))
 {
 	using namespace boost::numeric;
 
-	ublas::extents l;
-	BOOST_CHECK(l.empty());
-	BOOST_CHECK_EQUAL(l.size(), 0);
+	BOOST_REQUIRE_EQUAL(extents.size(),10);
 
-	auto l1 = ublas::extents{2,2};
+	BOOST_CHECK( extents[0].empty());
+	for(auto i = 1u; i < extents.size(); ++i)
+		BOOST_CHECK(!extents[i].empty());
 
-	BOOST_REQUIRE_EQUAL(l1.size(), 2);
-	BOOST_CHECK_EQUAL(l1[0], 2);
-	BOOST_CHECK_EQUAL(l1[1], 2);
+	BOOST_CHECK_EQUAL(extents[0].size(), 0);
 
-	auto l2 = ublas::extents{1,1};
+	for(auto i = 1u; i < 3u; ++i)
+		BOOST_CHECK_EQUAL(extents[i].size(), 1);
 
-	BOOST_REQUIRE_EQUAL(l2.size(), 2);
-	BOOST_CHECK_EQUAL(l2[0], 1);
-	BOOST_CHECK_EQUAL(l2[1], 1);
+	for(auto i = 3u; i < 7u; ++i)
+		BOOST_CHECK_EQUAL(extents[i].size(), 2);
 
-	auto l3 = ublas::extents{1,2};
+	for(auto i = 7u; i < 9u; ++i)
+		BOOST_CHECK_EQUAL(extents[i].size(), 3);
 
-	BOOST_REQUIRE_EQUAL(l3.size(), 2);
-	BOOST_CHECK_EQUAL(l3[0], 1);
-	BOOST_CHECK_EQUAL(l3[1], 2);
 
-	auto l4 = ublas::extents{2,1};
 
-	BOOST_REQUIRE_EQUAL(l4.size(), 2);
-	BOOST_CHECK_EQUAL(l4[0], 2);
-	BOOST_CHECK_EQUAL(l4[1], 1);
+	BOOST_CHECK_THROW( ublas::extents({1,0}), std::length_error );
+	BOOST_CHECK_THROW( ublas::extents({0}  ), std::length_error );
+	BOOST_CHECK_THROW( ublas::extents({0,1}), std::length_error );
+}
 
-	auto l5 = ublas::extents{2,2};
+BOOST_FIXTURE_TEST_CASE(test_extents_access, fixture,
+										 *boost::unit_test::label("extents")
+										 *boost::unit_test::label("access"))
+{
+	using namespace boost::numeric;
+	BOOST_REQUIRE_EQUAL(extents.size(),10);
 
-	BOOST_REQUIRE_EQUAL(l5.size(), 2);
-	BOOST_CHECK_EQUAL(l5[0], 2);
-	BOOST_CHECK_EQUAL(l5[1], 2);
+	BOOST_CHECK_EQUAL(extents[0].size(), 0);
 
-	auto l6 = ublas::extents{2,2,1};
+	BOOST_REQUIRE_EQUAL(extents[1].size(), 1);
+	BOOST_REQUIRE_EQUAL(extents[2].size(), 1);
 
-	BOOST_REQUIRE_EQUAL(l6.size(), 3);
-	BOOST_CHECK_EQUAL(l6[0], 2);
-	BOOST_CHECK_EQUAL(l6[1], 2);
-	BOOST_CHECK_EQUAL(l6[2], 1);
+	BOOST_CHECK_EQUAL (extents[1][0],1);
+	BOOST_CHECK_EQUAL (extents[2][0],2);
 
-	auto l7 = ublas::extents{2,2,3};
+	BOOST_REQUIRE_EQUAL(extents[3].size(), 2);
+	BOOST_REQUIRE_EQUAL(extents[4].size(), 2);
+	BOOST_REQUIRE_EQUAL(extents[5].size(), 2);
+	BOOST_REQUIRE_EQUAL(extents[6].size(), 2);
 
-	BOOST_REQUIRE_EQUAL(l7.size(), 3);
-	BOOST_CHECK_EQUAL(l7[0], 2);
-	BOOST_CHECK_EQUAL(l7[1], 2);
-	BOOST_CHECK_EQUAL(l7[2], 3);
+	BOOST_CHECK_EQUAL(extents[3][0],1);
+	BOOST_CHECK_EQUAL(extents[3][1],1);
 
-	auto l8 = ublas::extents{2,1,3,1};
+	BOOST_CHECK_EQUAL(extents[4][0],1);
+	BOOST_CHECK_EQUAL(extents[4][1],2);
 
-	BOOST_REQUIRE_EQUAL(l8.size(), 4);
-	BOOST_CHECK_EQUAL(l8[0], 2);
-	BOOST_CHECK_EQUAL(l8[1], 1);
-	BOOST_CHECK_EQUAL(l8[2], 3);
+	BOOST_CHECK_EQUAL(extents[5][0],2);
+	BOOST_CHECK_EQUAL(extents[5][1],1);
 
-	auto l9 = ublas::extents{2,1,3,1,4};
+	BOOST_CHECK_EQUAL(extents[6][0],2);
+	BOOST_CHECK_EQUAL(extents[6][1],3);
 
-	BOOST_REQUIRE_EQUAL(l9.size(), 5);
-	BOOST_CHECK_EQUAL(l9[0], 2);
-	BOOST_CHECK_EQUAL(l9[1], 1);
-	BOOST_CHECK_EQUAL(l9[2], 3);
-	BOOST_CHECK_EQUAL(l9[3], 1);
-	BOOST_CHECK_EQUAL(l9[4], 4);
+
+	BOOST_REQUIRE_EQUAL(extents[7].size(), 3);
+	BOOST_REQUIRE_EQUAL(extents[8].size(), 3);
+	BOOST_REQUIRE_EQUAL(extents[9].size(), 3);
+
+	BOOST_CHECK_EQUAL(extents[7][0],2);
+	BOOST_CHECK_EQUAL(extents[7][1],3);
+	BOOST_CHECK_EQUAL(extents[7][2],1);
+
+	BOOST_CHECK_EQUAL(extents[8][0],1);
+	BOOST_CHECK_EQUAL(extents[8][1],2);
+	BOOST_CHECK_EQUAL(extents[8][2],3);
+
+	BOOST_CHECK_EQUAL(extents[9][0],4);
+	BOOST_CHECK_EQUAL(extents[9][1],2);
+	BOOST_CHECK_EQUAL(extents[9][2],3);
+
+	BOOST_CHECK_NO_THROW( extents[6][2] );
+	BOOST_CHECK_NO_THROW( extents[9][3] );
 }
 
 BOOST_AUTO_TEST_SUITE_END();
--- test/tensor/test_layout.cpp
+++ test/tensor/test_layout.cpp
@@ -11,6 +11,7 @@
 
 BOOST_AUTO_TEST_SUITE(test_layout);
 
+
 BOOST_AUTO_TEST_CASE(test_layout_ctor,
 											*boost::unit_test::label("layout")
 											*boost::unit_test::label("constructor"))
@@ -21,11 +22,11 @@ BOOST_AUTO_TEST_CASE(test_layout_ctor,
 	BOOST_CHECK(l1.empty());
 	BOOST_CHECK_EQUAL(l1.size(), 0);
 
-	auto l2 = ublas::layout{{1u,2u,3u}};
+	auto l2 = ublas::layout{1u,2u,3u};
 	BOOST_CHECK(!l2.empty());
 	BOOST_CHECK_EQUAL(l2.size(), 3);
 
-	auto l3 = ublas::layout{{1,2,3}};
+	auto l3 = ublas::layout{1,2,3};
 	BOOST_CHECK(!l3.empty());
 	BOOST_CHECK_EQUAL(l3.size(), 3);
 
@@ -57,6 +58,49 @@ BOOST_AUTO_TEST_CASE(test_layout_ctor,
 	BOOST_CHECK(l9 == l3);
 
 	BOOST_CHECK( (ublas::layout{1,2,3} == l3) );
+}
+
+BOOST_AUTO_TEST_CASE(test_layout_access,
+											*boost::unit_test::label("layout")
+											*boost::unit_test::label("access"))
+{
+	using namespace boost::numeric;
+
+	auto l3 = ublas::layout{1,2,3};
+
+	auto l4 = l3;
+	BOOST_REQUIRE(!l4.empty());
+	BOOST_REQUIRE_EQUAL(l4.size(), 3);
+	BOOST_CHECK_EQUAL(l4[0],1);
+	BOOST_CHECK_EQUAL(l4[1],2);
+	BOOST_CHECK_EQUAL(l4[2],3);
+
+	auto l5 = ublas::layout{3,2,1};
+	BOOST_REQUIRE(!l5.empty());
+	BOOST_REQUIRE_EQUAL(l5.size(), 3);
+	BOOST_CHECK_EQUAL(l5[0],3);
+	BOOST_CHECK_EQUAL(l5[1],2);
+	BOOST_CHECK_EQUAL(l5[2],1);
+}
+
+BOOST_AUTO_TEST_CASE(test_layout_compare,
+												*boost::unit_test::label("layout")
+												*boost::unit_test::label("compare"))
+{
+	using namespace boost::numeric;
+
+	auto l3 = ublas::layout{1,2,3};
+	auto l5 = ublas::layout{3,2,1};
+
+	auto l7 = ublas::layout::first_order(3);
+	BOOST_CHECK(l7 == l3);
+
+	auto l8 = ublas::layout::last_order(3);
+	BOOST_CHECK(l8 == l5);
+	BOOST_CHECK(l8 != l3);
+
+	auto l9 = ublas::layout{1,2,3};
+	BOOST_CHECK(l9 == l3);
 }
 
 BOOST_AUTO_TEST_SUITE_END();
