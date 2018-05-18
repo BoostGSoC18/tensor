da5081f46ab4626becd389d9d9dc29062932da7d
Cem Bassoy <cem.bassoy@iosb.fraunhofer.de>
2018-05-18 11:33:25 +0200

    bug removed in tensor access function.

--- include/boost/numeric/ublas/tensor/tensor.hpp
+++ include/boost/numeric/ublas/tensor/tensor.hpp
@@ -449,7 +449,7 @@ public:
 		if constexpr (sizeof...(is) == 0)
 			return this->data_[i];
 		else
-			return this->data_[detail::access<0ul>(0ul,i,is...)];
+			return this->data_[detail::access<0ul>(0ul,this->strides_,i,is...)];
 	}
 
 
--- test/tensor/test_tensor_matrix_interoperability.cpp
+++ test/tensor/test_tensor_matrix_interoperability.cpp
@@ -94,7 +94,7 @@ BOOST_FIXTURE_TEST_CASE_TEMPLATE( test_tensor_matrix_interoperability_copy_ctor_
 		check(e);
 }
 
-#if 0
+
 
 BOOST_FIXTURE_TEST_CASE_TEMPLATE( test_tensor_matrix_interoperability_copy_assignment, value,  test_types, fixture )
 {
@@ -109,23 +109,28 @@ BOOST_FIXTURE_TEST_CASE_TEMPLATE( test_tensor_matrix_interoperability_copy_assig
 		assert(e.size() == 2);
 		auto t = tensor_type{};
 		auto r = matrix_type(e[0],e[1]);
+		std::iota(r.data().begin(),r.data().end(), 1);
 		t = r;
-		BOOST_CHECK_EQUAL (  t.size() , r.size() );
-		BOOST_CHECK_EQUAL (  t.rank() , r.rank() );
-		BOOST_CHECK ( t.strides() == r.strides() );
-		BOOST_CHECK ( t.extents() == r.extents() );
+
+		BOOST_CHECK_EQUAL (  t.extents().at(0) , e.at(0) );
+		BOOST_CHECK_EQUAL (  t.extents().at(1) , e.at(1) );
+		BOOST_CHECK_EQUAL (  t.size() , e.product() );
+		BOOST_CHECK_EQUAL (  t.rank() , e.size() );
 		BOOST_CHECK       ( !t.empty()    );
 		BOOST_CHECK_NE    (  t.data() , nullptr);
 
-		for(auto i = 0ul; i < t.size(); ++i)
-			BOOST_CHECK_EQUAL( t[i], r[i]  );
+		for(auto j = 0ul; j < t.size(1); ++j){
+			for(auto i = 0ul; i < t.size(0); ++i){
+				BOOST_CHECK_EQUAL( t.at(i,j), r(i,j)  );
+			}
+		}
 	};
 
 	for(auto const& e : extents)
 		check(e);
 }
 
-
+#if 0
 BOOST_FIXTURE_TEST_CASE_TEMPLATE( test_tensor_matrix_interoperability_move_assignment, value,  test_types, fixture )
 {
 	using namespace boost::numeric;
@@ -156,3 +161,4 @@ BOOST_FIXTURE_TEST_CASE_TEMPLATE( test_tensor_matrix_interoperability_move_assig
 
 
 BOOST_AUTO_TEST_SUITE_END();
+
