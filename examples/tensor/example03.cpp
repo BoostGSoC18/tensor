8c1befb27676c08e63039facd6aaa7ffd4a2cbd4
Cem Bassoy <cem.bassoy@gmail.com>
2018-05-25 14:18:10 +0200

    transposition in tensor/example03.cpp added.

--- examples/tensor/example03.cpp
+++ examples/tensor/example03.cpp
@@ -24,6 +24,7 @@ int main()
 	using matrixf = matrix<float,format>;
 	using vectorf = vector<float>;
 
+	// Tensor-Vector-Multiplications - Including Transposition
 	{
 
 		auto n = shape{3,4,2};
@@ -38,9 +39,12 @@ int main()
 		q = 2u;
 		tensorf C2 = prod(q,A,vectorf(n[q-1],1)) + 4;
 
-		// C3() = A(i,j,k)*T1(i)*T2(j)*T2(k);
-		q = 3u;
+		// C3() = A(i,j,k)*T1(i)*T2(j)*T2(k);		
 		tensorf C3 = prod(1,prod(1,prod(1,A,vectorf(n[0],1)),vectorf(n[1],1)),vectorf(n[2],1));
+
+		// C4(i,j) = A(k,i,j)*T1(k) + 4;
+		q = 1u;
+		tensorf C4 = prod(q, trans(A,{2,3,1}),vectorf(n[2],1)) + 4;
 
 
 		// formatted output
@@ -61,9 +65,16 @@ int main()
 		std::cout << "% C3() = A(i,j,k)*T1(i)*T2(j)*T2(k);" << std::endl << std::endl;
 		std::cout << "C3()=" << C3(0) << ";" << std::endl << std::endl;
 
+		// formatted output
+		std::cout << "% --------------------------- " << std::endl;
+		std::cout << "% --------------------------- " << std::endl << std::endl;
+		std::cout << "% C4(i,j) = A(k,i,j)*T1(k) + 4;" << std::endl << std::endl;
+		std::cout << "C4=" << C4 << ";" << std::endl << std::endl;
+
 	}
 
 
+	// Tensor-Matrix-Multiplications - Including Transposition
 	{
 
 		auto n = shape{3,4,2};
@@ -86,6 +97,9 @@ int main()
 		// C4(i,l1,l2) = A(i,j,k)*T2(l2,k)*T1(l1,j);
 		tensorf C4 = prod(q-1,prod(q,A,matrixf(m+2,n[q-1],1)),matrixf(m+1,n[q-2],1));
 
+		// C5(i,k,l) = A(i,k,j)*T1(l,j) + 4;
+		q = 3u;
+		tensorf C5 = prod(q,trans(A,{1,3,2}),matrixf(m,n[1],1)) + 4;
 
 		// formatted output
 		std::cout << "% --------------------------- " << std::endl;
@@ -112,5 +126,11 @@ int main()
 		std::cout << "C4=" << C4 << ";" << std::endl << std::endl;
 		std::cout << "% C3 and C4 should have the same values, true? " << std::boolalpha << (C3 == C4) << "!" << std::endl;
 
+
+		// formatted output
+		std::cout << "% --------------------------- " << std::endl;
+		std::cout << "% --------------------------- " << std::endl << std::endl;
+		std::cout << "% C5(i,k,l) = A(i,k,j)*T1(l,j) + 4;" << std::endl << std::endl;
+		std::cout << "C5=" << C5 << ";" << std::endl << std::endl;
 	}
 }
